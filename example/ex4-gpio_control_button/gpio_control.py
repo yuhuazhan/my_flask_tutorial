75bdc1d7ad71b26b64db0e3e436e1c9c3ab9b296
yuhuazhan <net73751@gmail.com>
2017-03-30 11:08:03 +0800

    add Open and close button  on ex4-gpio_control add function to handle form post method

--- example/ex4-gpio_control_button/gpio_control.py
+++ example/ex4-gpio_control_button/gpio_control.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect, flash
 
 #Windows
 # $ set FLASK_DEBUG=1	                        #enable debug 
-# $ set FLASK_APP=ex2-hellow_without_app_run.py
+# $ set FLASK_APP=gpio_control.py
 # $ flask run
 
 #Unix
@@ -28,19 +28,15 @@ from flask import Flask, render_template, request, url_for, redirect, flash
 # 127.0.0.1 - - [24/Mar/2017 13:38:17] "GET / HTTP/1.1" 200 -
 # * Detected change in 'F:\\Google Drive\\碩士論文\\flask\\example\\ex2-hellow_without_app_run\\ex2-hellow_without_app_run.py', reloading
 app = Flask(__name__)
-app.secret_key = 'some_secret'
 @app.route('/hello/',methods=["GET","POST"])
 def hello():
     error = ''
     try:
         if request.method =="POST":
-            attempted_username = request.form['username']
-            attempted_password = request.form['password']
-			
-            flash(attempted_username)
-            flash(attempted_password)
-			
-            error = 'Invaild credentials Try again!!'
+            if request.form.get('Open') =='Open':
+                error = 'Open'
+            elif request.form.get('Close')=='Close':
+                error = 'Close'
         return render_template('gpio_control.html',error=error)
     except Exception as e:
         flash(e)
